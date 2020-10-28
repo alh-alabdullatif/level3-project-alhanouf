fa604edc1cf3ba9c353c8d7332efde351ef3c68f
Ubuntu <ubuntu@ip-10-0-1-182.me-south-1.compute.internal>
2020-10-28 20:40:50 +0000

    changed dir in runner

--- e2e-js-test/runner.sh
+++ e2e-js-test/runner.sh
@@ -6,12 +6,12 @@ files=$testfile
 
 if [ -z $testfile ]
 then
-  files=$(ls tests/*_test.js)
+  files=$(ls *_test.js)
 fi
 
 for test in $files
 do
-  node tests/test_helper.js
+  node test_helper.js
   casperjs test $test
   ret=$?
   if [ ! $ret == "0" ]; then code=1; fi
